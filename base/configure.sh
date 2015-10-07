1fcf8ac4f8c53e463e37572afc8d36385e3809d3
arrawatia <sumit.arrawatia@gmail.com>
2015-10-07 10:38:37 -0700

    Fix config issues.

--- base/configure.sh
+++ base/configure.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
-cat >> conf/storm.yaml <<EOF
+cat <<- EOF > /opt/apache-storm/conf/storm.yaml
 storm.local.dir: "/tmp"
-EOF
-
-if [ -n "$1" ]; then
-   cat >> conf/storm.yaml <<EOF
 storm.exhibitor.servers:
-- "$EXHIBITOR_SERVERS"
-storm.exhibitor.port: $EXHIBITOR_PORT"
-storm.exhibitor.retry.times: "10"
-storm.exhibitor.retry.interval : "10"
-storm.exhibitor.retry.intervalceiling.millis: "1000"
+- $EXHIBITOR_SERVERS
+storm.exhibitor.port: $EXHIBITOR_PORT
+storm.exhibitor.retry.times: 10
+storm.exhibitor.retry.interval : 10
+storm.exhibitor.retry.intervalceiling.millis: 1000
+storm.exhibitor.poll.millis: 10000
+EOF
+
+
+if [ -n "$NIMBUS_HOST" ]; then
+  cat <<- EOF >> /opt/apache-storm/conf/storm.yaml
+nimbus.host: $NIMBUS_HOST
 EOF
 fi
 
-if [ -n "$2" ]; then
-   cat >> conf/storm.yaml <<EOF
-nimbus.host: "$NIMBUS_HOST"
-EOF
-fi
-
+echo "============== STORM CONFIG: ============="
 cat conf/storm.yaml
